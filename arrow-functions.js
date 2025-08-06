0db89bab77f27305d11d9f3d11f96e65f407ce40
andreaspe76 <andreaspergantis@hotmail.com>
2025-08-06 11:37:43 +0300

    Start forst lesson in advance js

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => a + b;
+// let sum2 = addTwoNumbers(3, 5);
+// console.log(sum2);
 
 // Implicit Returns
+const saySomething = msg => console.log(msg);
+saySomething('Hello, there!');
+
+const sayHello = () => console.log('Hello, there!');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiple line string!
+    </p>`
+)
+console.log(returnMultipleLines());
